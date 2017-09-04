59867c50e6946a05b2960eea5b57c674d88eff89
aiyu852 <1205105078@qq.com>
2017-09-05 01:05:13 +0800

    09-05

---
+++ 5.15.cpp
@@ -0,0 +1,20 @@
+/* pound.c -- 定义带有一个参数的函数*/
+#include <stiod.h>
+void pound(int n);
+int main(void)
+{
+	int time = 5;
+	char ch = '!';
+	float f = 6.0;
+	pound (times);
+	pound (ch);
+	pound ((int) f);
+	return 0;
+} 
+
+void pound (int n)
+{
+	while (n-- > 0)
+		printf("#");
+	printf("\n");ss
+}
